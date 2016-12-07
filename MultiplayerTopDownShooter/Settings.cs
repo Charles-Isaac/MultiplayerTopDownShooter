8edc3e7fadb4ac3aa25baab075a02ae9a0dbdb3f
clonecharle1 <charl_i_c@hotmail.com>
2016-12-07 10:55:43 -0500

    Updates murs

--- MultiplayerTopDownShooter/Settings.cs
+++ MultiplayerTopDownShooter/Settings.cs
@@ -10,7 +10,7 @@ namespace ClonesEngine
         private const byte m_DeFaultBulletSpeed = 15;
         private const byte m_DefaltBulletSize = 18;//12
         private const byte m_PlayerSize = 80;
-        private const int m_NumberOfBuilding = 30;
+        private const int m_NumberOfBuilding = 16;
 
         private const int m_GameSizeX = 2000;
         private const int m_GameSizeY = 2000;
